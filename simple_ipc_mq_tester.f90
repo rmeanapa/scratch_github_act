968f7b5abff6290ba71fbac2d93932076e94dc10
Ruben Meana-Paneda <rubenmepa@gmail.com>
2026-04-28 11:28:33 -0400

    skip tester

--- simple_ipc_mq_tester.f90
+++ simple_ipc_mq_tester.f90
@@ -50,7 +50,9 @@ contains
 
   ! Run all ipc_mq unit tests. Linux-only; compiled to a no-op elsewhere.
   subroutine run_all_ipc_mq_tests()
-#if defined(__linux__) && !defined(_WIN32)
+#ifdef _WIN32
+    return
+#elif defined(__linux__)
     write(*,'(A)') '**** running all ipc mq tests ****'
     call test_create_and_kill()
     call test_create_and_kill_maxmsg()
